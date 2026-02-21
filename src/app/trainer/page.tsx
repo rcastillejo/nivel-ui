7bd66fb6410bc5c20a95264906b8a5829e23896f
ricardo.castillejo <ricardo.castillejo@gmail.com>
2026-02-21 14:42:44 -0500

    Cambiar nombre en el resitro de disponibilidad

--- src/app/trainer/page.tsx
+++ src/app/trainer/page.tsx
@@ -42,7 +42,7 @@ export default function TrainerDashboard() {
                     : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 }`}
               >
-                Registrar Horario
+                Registra tu disponibilidad
               </button>
               <button
                 onClick={() => setCurrentView('appointments')}
